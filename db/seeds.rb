26457b5c8ab63350b0234ee59714dbf0cc77521c
crabtree <justin.kyle.crabtree@gmail.com>
2019-09-02 14:41:19 -0400

    adds restful actions

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+contact = Contact.new(first_name: "John", last_name: "Wick", phone_number: "222-222-2222"
+  )
+contact.save
+
+contact = Contact.new(first_name: "Bruce", last_name: "Wayne", phone_number: "333-333-3333"
+  )
+contact.save
+
+contact = Contact.new(first_name: "Clark", last_name: "Kent", phone_number: "444-444-4444"
+  )
+contact.save
